5c308beb88e1e865c4dad5dcc5437567f94d5e17
Rashmi-278 <rashmiabbigeri@gmail.com>
2024-05-14 10:52:38 +0530

    use schema_id

--- main.py
+++ main.py
@@ -2,14 +2,10 @@ from flask import Flask, jsonify, redirect, url_for, request
 import requests
 import json
 from datetime import datetime
-from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app) 
-
-# CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
-
+CORS(app)
 
 def convert_unix_to_utc(unix_time):
     """Converts UNIX timestamp to a UTC datetime string."""
@@ -42,7 +38,6 @@ def fetch_attestations(attester_address):
         return response.json().get('data', {}).get('attestations', [])
     else:
         raise Exception(f"Query failed with status code {response.status_code}")
-    
 
 def fetch_schema_details(schema_id):
     query = '''
@@ -63,7 +58,7 @@ def fetch_schema_details(schema_id):
     '''
     variables = {
         "schemaWhere2": {
-            "id": '0x25eb07102ee3f4f86cd0b0c4393457965b742b8acc94aa3ddbf2bc3f62ed1381'
+            "id": schema_id
         }
     }
     response = requests.post(
@@ -75,9 +70,7 @@ def fetch_schema_details(schema_id):
         return response.json().get('data', {}).get('schema', {})
     else:
         raise Exception(f"Query failed with status code {response.status_code}")
-    
 
-# Get the Schema's registered to DAO Registration factory by Attester Address
 @app.route('/attestations/<attester_address>', methods=['GET'])
 def get_attestations(attester_address):
     if not attester_address:
@@ -86,7 +79,6 @@ def get_attestations(attester_address):
     try:
         attestations_data = fetch_attestations(attester_address)
 
-        # Check if the response from GraphQL is empty
         if not attestations_data:
             return jsonify({"message": "No attestations made by this Issuer"}), 404
 
@@ -116,11 +108,9 @@ def get_attestations(attester_address):
                 elif key_name in array_fields and isinstance(value, list):
                     array_fields[key_name].extend(value)
 
-            # Fetch and integrate schema details
             for i in range(len(array_fields["schemaUID"])):
-                schema_id = array_fields['schemaUID'][i];
+                schema_id = array_fields['schemaUID'][i]
                 schema_details = fetch_schema_details(schema_id) or {}
-                print (schema_details)
                 structured_schemas_by_attester.append({
                     "schemaUID": array_fields['schemaUID'][i],
                     "schemaDescription": array_fields['schemaDescription'][i],
@@ -132,7 +122,7 @@ def get_attestations(attester_address):
                         "revocable": schema_details.get("revocable", False),
                         "schema": schema_details.get("schema", ""),
                         "attestationsCount": schema_details.get("_count", {}).get("attestations", 0),
-                        "time": convert_unix_to_utc(schema_details.get("time", "")),
+                        "time": convert_unix_to_utc(schema_details.get("time", 0)),
                         "txid": schema_details.get("txid", "")
                     }
                 })
@@ -148,9 +138,8 @@ def get_attestations(attester_address):
         return jsonify(structured_data)
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-    
+
 def fetch_schema_attestations(schema_id):
-    """Fetches attestations for a given schema ID."""
     query = '''
     query ($schemaWhere2: SchemaWhereUniqueInput!) {
       schema(where: $schemaWhere2) {
@@ -175,7 +164,7 @@ def fetch_schema_attestations(schema_id):
 def get_schema_attestations(schema_id):
     try:
         raw_attestations = fetch_schema_attestations(schema_id)
-        unique_attesters = {attestation['attester'] for attestation in raw_attestations}  # Use a set for unique addresses
+        unique_attesters = {attestation['attester'] for attestation in raw_attestations}
 
         results = []
 
@@ -184,21 +173,19 @@ def get_schema_attestations(schema_id):
 
             if attester_response.status_code == 200:
                 attester_data = attester_response.get_json()
-                attester_data['attesterAddress'] = attester_address  # Add attester address to the response data
+                attester_data['attesterAddress'] = attester_address
                 results.append(attester_data)
             else:
                 results.append({
                     "error": f"Failed to fetch data for attester {attester_address}",
                     "statusCode": attester_response.status_code,
-                    "attesterAddress": attester_address  # Include attester address even in case of error
+                    "attesterAddress": attester_address
                 })
 
         return jsonify(results)
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-
-    
 @app.route('/', methods=['GET'])
 def get_home():
     docs_html = """
@@ -222,13 +209,9 @@ def get_home():
     """
     return docs_html
 
-
-# Redirect any other endpoint to '/'
 @app.route('/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def catch_all(path):
     return redirect(url_for('get_home'))
-
-
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
