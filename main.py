4016aa8e4aca925b40818171f7b423467308e7eb
Rashmi-278 <rashmiabbigeri@gmail.com>
2024-05-15 17:37:17 +0530

    context schema id

--- main.py
+++ main.py
@@ -85,8 +85,8 @@ def extract_daoip7_schemas(attestations):
     return list(unique_schemas)
 
 # Example usage:
-schema_id = "0xcc6c9b07bfccd15c8f313d23bf4389fb75629a620c5fa669c898bf1e023f2508" #Context schema on OP Mainnet
-daoip7_schemas = populate_daoip7_compliant_schemas(schema_id)
+context_schema_id = "0xcc6c9b07bfccd15c8f313d23bf4389fb75629a620c5fa669c898bf1e023f2508" #Context schema on OP Mainnet
+daoip7_schemas = populate_daoip7_compliant_schemas(context_schema_id)
 print("DAOIP7 Compliant Schema IDs:", daoip7_schemas)
 
 
@@ -155,7 +155,7 @@ def get_attestations(attester_address):
 
     try:
         attestations_data = fetch_attestations(attester_address)
-        daoip7_schemas = populate_daoip7_compliant_schemas(schema_id)
+        daoip7_schemas = populate_daoip7_compliant_schemas(context_schema_id)
 
 
         if not attestations_data:
@@ -248,7 +248,7 @@ def fetch_schema_attestations(schema_id):
 def get_schema_attestations(schema_id):
     try:
         raw_attestations = fetch_schema_attestations(schema_id)
-        daoip7_schemas = populate_daoip7_compliant_schemas(schema_id)
+        daoip7_schemas = populate_daoip7_compliant_schemas(context_schema_id)
         unique_attesters = {attestation['attester'] for attestation in raw_attestations}
 
         results = []
