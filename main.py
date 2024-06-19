462f0fe57cb8eb2d631d94a5da938e139680f4fa
Rashmi-278 <rashmiabbigeri@gmail.com>
2024-06-19 17:19:58 +0530

    prod redis

--- main.py
+++ main.py
@@ -12,11 +12,11 @@ from schema_ids import SchemaIDs
 
 app = Flask(__name__)
 CORS(app)
-# redis_url = 'redis://red-cp1lchmct0pc73d37gl0:6379'
-# r = redis.Redis.from_url(redis_url, db=0, decode_responses=True)
+redis_url = 'redis://red-cp1lchmct0pc73d37gl0:6379'
+r = redis.Redis.from_url(redis_url, db=0, decode_responses=True)
 
-redis_url = 'localhost'
-r = redis.Redis(host=redis_url, port=6379, db=0, decode_responses=True)
+# redis_url = 'localhost'
+# r = redis.Redis(host=redis_url, port=6379, db=0, decode_responses=True)
 
 urls = EASGraphQLURLs()
 
