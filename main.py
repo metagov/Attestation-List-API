9fbfd4fbc0b46e58bd9b52288c1a8d95f1024868
Rashmi-278 <rashmiabbigeri@gmail.com>
2024-06-26 12:12:45 +0530

    add blacklist, add caching and refresh it

--- main.py
+++ main.py
@@ -12,15 +12,41 @@ from schema_ids import SchemaIDs
 
 app = Flask(__name__)
 CORS(app)
-redis_url = 'redis://red-cp1lchmct0pc73d37gl0:6379'
-r = redis.Redis.from_url(redis_url, db=0, decode_responses=True)
+# redis_url = 'redis://red-cp1lchmct0pc73d37gl0:6379'
+# r = redis.Redis.from_url(redis_url, db=0, decode_responses=True)
 
-# redis_url = 'localhost'
-# r = redis.Redis(host=redis_url, port=6379, db=0, decode_responses=True)
+redis_url = 'localhost'
+r = redis.Redis(host=redis_url, port=6379, db=0, decode_responses=True)
 
 urls = EASGraphQLURLs()
 
-print(urls.get_url_by_network_id('42161'))  # Should print False if no such ID exists
+#print(urls.get_url_by_network_id('42161'))  # Should print False if no such ID exists
+
+
+def add_to_blacklist(address):
+    r.sadd("blacklisted_attesters", address)
+
+def is_blacklisted(address):
+    return r.sismember("blacklisted_attesters", address)
+
+
+@app.route('/blacklist/<attester_address>', methods=['POST', 'DELETE', 'GET'])
+def manage_blacklist(attester_address):
+    if request.method == 'GET':
+        add_to_blacklist(attester_address)
+
+        if is_blacklisted(attester_address):
+            return jsonify({"status": "Address is blacklisted"}), 200
+        else:
+            return jsonify({"status": "Address is not blacklisted"}), 200
+    elif request.method == 'POST':
+        add_to_blacklist(attester_address)
+        return jsonify({"status": "Address added to blacklist"}), 200
+    elif request.method == 'DELETE':
+        r.srem("blacklisted_attesters", attester_address)
+        return jsonify({"status": "Address removed from blacklist"}), 200
+
+
 
 def test_redis():
     try:
@@ -36,7 +62,6 @@ test_redis()
 
 try:
     response = r.ping()
-    print("Redis is connected:", response)
 except redis.exceptions.ConnectionError as e:
     print("Redis connection error:", str(e))
 
@@ -119,10 +144,14 @@ def extract_daoip7_schemas(attestations):
 # Example usage:
 context_schema_id = "0xcc6c9b07bfccd15c8f313d23bf4389fb75629a620c5fa669c898bf1e023f2508" #Context schema on OP Mainnet
 daoip7_schemas = populate_daoip7_compliant_schemas(context_schema_id, 10) 
-print("DAOIP7 Compliant Schema IDs populated of OP network:", daoip7_schemas)
+#print("DAOIP7 Compliant Schema IDs populated of OP network:", daoip7_schemas)
 
 
 def fetch_attestations(attester_address, networkId = 10):
+    if is_blacklisted(attester_address):
+        app.logger.error("This attester address is blacklisted.")
+        return []
+    
     query = '''
     query Attestations($attesterAddress: [String!]) {
       attestations(where: {
@@ -189,108 +218,12 @@ def fetch_schema_details(schema_id, networkId = 10):
     else:
         raise Exception(f"fetch_schema_details Query failed with status code {response.status_code}")
 
-# @app.route('/attestations/<attester_address>', methods=['GET'])
-def get_attestations(attester_address):
-    if not attester_address:
-        return {"error": "Attester address is required."}, 400
-
-    try:
-        attestations_data = fetch_attestations(attester_address)
-        if not attestations_data:
-            return {"message": "No attestations made by this Issuer"}, 404
-
-
-        structured_schemas_by_attester = []
-        for attestation in attestations_data:
-            decoded_data_list = json.loads(attestation['decodedDataJson'])
-            if not decoded_data_list:
-                app.logger.error("Missing 'decodedDataJson' in attestation: {}".format(attestation))
-                continue
-            non_array_fields = {
-                "issuerName": "",
-                "issuerDescription": "",
-                "logo": "",
-                "apiDocsURI": "",
-                "attesterAddress": ""
-            }
-
-            array_fields = {
-                "schemaUID": [],
-                "schemaDescription": [],
-                "networkID": []
-            }
-
-            for decoded_data_item in decoded_data_list:
-                key_name = decoded_data_item['name']
-                value = decoded_data_item['value']['value']
-
-                if key_name in non_array_fields:
-                    non_array_fields[key_name] = value
-                elif key_name in array_fields and isinstance(value, list):
-                    array_fields[key_name].extend(value)
-
-            for i in range(len(array_fields["schemaUID"])):
-                schema_id = array_fields['schemaUID'][i]
-                network_id = int(array_fields['networkID'][i]['hex'],16)
-                daoip7_schemas = populate_daoip7_compliant_schemas(context_schema_id, network_id)
-                schema_details = fetch_schema_details(schema_id, network_id) or {}
-                print(schema_details)
-                if (not schema_details):  # Skip if schema_details is empty
-                    continue  
-
-                if schema_id in daoip7_schemas: # Context set check   # Network agnostic check      
-                    structured_schemas_by_attester.append({
-                        "schemaUID": array_fields['schemaUID'][i],
-                        "schemaDescription": array_fields['schemaDescription'][i],
-                        "networkID": array_fields['networkID'][i],
-                        "schemaDetails": {
-                            "creator": schema_details.get("creator", ""),
-                            "id": schema_details.get("id", ""),
-                            "resolver": schema_details.get("resolver", ""),
-                            "revocable": schema_details.get("revocable", False),
-                            "schema": schema_details.get("schema", ""),
-                            "attestationsCount": schema_details.get("_count", {}).get("attestations", 0),
-                            "time": convert_unix_to_utc(schema_details.get("time", 0)),
-                            "txid": schema_details.get("txid", "")
-                        }
-                    })
-                else:
-                    schema_text = schema_details.get("schema", "{}")
-                    if contains_word_context(schema_text):
-                        structured_schemas_by_attester.append({
-                            "schemaUID": schema_id,
-                            "schemaDescription": array_fields['schemaDescription'][i],
-                            "networkID": array_fields['networkID'][i],
-                            "schemaDetails": {
-                                "creator": schema_details.get("creator", ""),
-                                "id": schema_details.get("id", ""),
-                                "resolver": schema_details.get("resolver", ""),
-                                "revocable": schema_details.get("revocable", False),
-                                "schema": schema_details.get("schema", ""),
-                                "attestationsCount": schema_details.get("_count", {}).get("attestations", 0),
-                                "time": convert_unix_to_utc(schema_details.get("time", 0)),
-                                "txid": schema_details.get("txid", "")
-                            }
-                        })
-                    else:
-                        continue  # Optionally handle cases where 'context' is not found
-
-        structured_data = {
-            "issuerName": non_array_fields['issuerName'],
-            "issuerDescription": non_array_fields['issuerDescription'],
-            "logo": non_array_fields['logo'],
-            "apiDocsURI": non_array_fields['apiDocsURI'],
-            "schemas": structured_schemas_by_attester,
-        }
-
-        return structured_data, 200
-    except Exception as e:
-        return {"error in get_attestations": str(e)}, 500
-    
-
 
 @app.route('/attestations/<attester_address>', methods=['GET'])
 def get_attestations_new(attester_address,refresh=None):
+    if is_blacklisted(attester_address):
+        return {"error": "This attester address is blacklisted."}, 403
+    
     if not attester_address:
         return {"error": "Attester address is required."}, 400
 
@@ -317,6 +250,7 @@ def get_attestations_new(attester_address,refresh=None):
 
         # Fetch new data if not in cache or cache is expired
         attestations_data = fetch_attestations(attester_address)
+        print(attestations_data)
         if not attestations_data:
             return {"message": "No attestations made by this Issuer"}, 404
 
@@ -340,45 +274,38 @@ def get_attestations_new(attester_address,refresh=None):
                 "networkID": []
             }
 
+        
             for decoded_data_item in decoded_data_list:
-                key_name = decoded_data_item['name']
-                value = decoded_data_item['value']['value']
+                try:
+                    key_name = decoded_data_item['name']
+                    # Ensure that 'value' exists and is a dictionary before accessing its 'value' key
+                    if 'value' in decoded_data_item and isinstance(decoded_data_item['value'], dict):
+                        value = decoded_data_item['value'].get('value')
+                        if key_name in non_array_fields:
+                            non_array_fields[key_name] = value
+                        elif key_name in array_fields and isinstance(value, list):
+                            array_fields[key_name].extend(value)
+                    else:
+                        raise ValueError(f"Expected 'value' to be a dictionary in decoded_data_item, found: {decoded_data_item['value']}")
 
-                if key_name in non_array_fields:
-                    non_array_fields[key_name] = value
-                elif key_name in array_fields and isinstance(value, list):
-                    array_fields[key_name].extend(value)
+                except Exception as e:
+                    app.logger.error(f"Error processing decoded_data_item: {decoded_data_item}. Error: {str(e)}")
+                    continue  # Optionally skip this iteration or handle the error as needed
+
 
             for i in range(len(array_fields["schemaUID"])):
-                schema_id = array_fields['schemaUID'][i]
-                network_id = int(array_fields['networkID'][i]['hex'],16)
-                daoip7_schemas = populate_daoip7_compliant_schemas(context_schema_id, network_id)
-                schema_details = fetch_schema_details(schema_id, network_id) or {}
-                print(schema_details)
-                if (not schema_details):  # Skip if schema_details is empty
-                    continue  
+                try:
+                    schema_id = array_fields['schemaUID'][i]
+                    network_id = int(array_fields['networkID'][i]['hex'], 16)
+                    daoip7_schemas = populate_daoip7_compliant_schemas(context_schema_id, network_id)
+                    schema_details = fetch_schema_details(schema_id, network_id) or {}
+                    print(schema_details)
+                    if not schema_details:  # Skip if schema_details is empty
+                        continue
 
-                if schema_id in daoip7_schemas: # Context set check   # Network agnostic check      
-                    structured_schemas_by_attester.append({
-                        "schemaUID": array_fields['schemaUID'][i],
-                        "schemaDescription": array_fields['schemaDescription'][i],
-                        "networkID": array_fields['networkID'][i],
-                        "schemaDetails": {
-                            "creator": schema_details.get("creator", ""),
-                            "id": schema_details.get("id", ""),
-                            "resolver": schema_details.get("resolver", ""),
-                            "revocable": schema_details.get("revocable", False),
-                            "schema": schema_details.get("schema", ""),
-                            "attestationsCount": schema_details.get("_count", {}).get("attestations", 0),
-                            "time": convert_unix_to_utc(schema_details.get("time", 0)),
-                            "txid": schema_details.get("txid", "")
-                        }
-                    })
-                else:
-                    schema_text = schema_details.get("schema", "{}")
-                    if contains_word_context(schema_text):
+                    if schema_id in daoip7_schemas:  # Context set check   # Network agnostic check      
                         structured_schemas_by_attester.append({
-                            "schemaUID": schema_id,
+                            "schemaUID": array_fields['schemaUID'][i],
                             "schemaDescription": array_fields['schemaDescription'][i],
                             "networkID": array_fields['networkID'][i],
                             "schemaDetails": {
@@ -393,7 +320,29 @@ def get_attestations_new(attester_address,refresh=None):
                             }
                         })
                     else:
-                        continue  # Optionally handle cases where 'context' is not found
+                        schema_text = schema_details.get("schema", "{}")
+                        if contains_word_context(schema_text):
+                            structured_schemas_by_attester.append({
+                                "schemaUID": schema_id,
+                                "schemaDescription": array_fields['schemaDescription'][i],
+                                "networkID": array_fields['networkID'][i],
+                                "schemaDetails": {
+                                    "creator": schema_details.get("creator", ""),
+                                    "id": schema_details.get("id", ""),
+                                    "resolver": schema_details.get("resolver", ""),
+                                    "revocable": schema_details.get("revocable", False),
+                                    "schema": schema_details.get("schema", ""),
+                                    "attestationsCount": schema_details.get("_count", {}).get("attestations", 0),
+                                    "time": convert_unix_to_utc(schema_details.get("time", 0)),
+                                    "txid": schema_details.get("txid", "")
+                                }
+                            })
+                        else:
+                            continue  # Optionally handle cases where 'context' is not found
+                except Exception as e:
+                    app.logger.error(f"Error processing schema ID {schema_id} at index {i}: {str(e)}")
+                    continue  # Optionally skip this iteration or handle the error as needed
+
         structured_data = {
             "issuerName": non_array_fields['issuerName'],
             "issuerDescription": non_array_fields['issuerDescription'],
@@ -432,47 +381,6 @@ def fetch_schema_attestations(schema_id, network_id=10):
     else:
         raise Exception(f"fetch_schema_attestations Query failed with status code {response.status_code}")
 
-# @app.route('/schema_attestations/<schema_id>', methods=['GET'])
-def get_schema_attestations(schema_id):
-    try:
-        raw_attestations = fetch_schema_attestations(schema_id)
-        daoip7_schemas = populate_daoip7_compliant_schemas(context_schema_id)
-        unique_attesters = {attestation['attester'] for attestation in raw_attestations}
-
-        results = []
-
-        for attester_address in unique_attesters:
-            attester_response, status_code = get_attestations(attester_address)  # Call and unpack the tuple
-
-            if status_code == 200:
-                attester_data = attester_response
-                print("success")
-
-                # Filter schemas to include only those where the creator matches the attester address
-                attester_data['schemas'] = [
-                    schema for schema in attester_data['schemas']
-                    if schema['schemaDetails']['creator'] == attester_address
-                ]
-
-                # Check if there are valid schemas after filtering; if not, continue to the next attester
-                if not attester_data['schemas']:
-                    continue
-
-                attester_data['attesterAddress'] = attester_address
-                results.append(attester_data)
-            else:
-                print('fail')
-                results.append({
-                    "error": f"Failed to fetch data for attester {attester_address}",
-                    "statusCode": status_code,
-                    "attesterAddress": attester_address
-                })
-
-        return jsonify(results), 200
-    except Exception as e:
-        return jsonify({"error in get_schema_attestations": str(e)}), 500
-    
-
 
 
 @app.route('/schema_attestations/<schema_id>', methods=['GET'])
@@ -481,27 +389,41 @@ def get_schema_attestations_new(schema_id, refresh=None):
         refresh = request.args.get('refresh', 'false').lower() == 'true'
 
     cache_key = f"schema_attestations_{schema_id}"
+    no_data_cache_key = f"no_data_schema_{schema_id}"
     
     try:
         if not refresh:
+            # Check if this schema ID has been cached as having no data
+            if r.get(no_data_cache_key):
+                print("No data previously found for this schema ID, skipping fetch.")
+                return jsonify({"message": "No data found for this schema ID previously."}), 404
+            
+            # Attempt to return cached data if available
             cached_data = r.get(cache_key)
             if cached_data:
                 print("Returning cached data.")
                 return jsonify(json.loads(cached_data)), 200
             else:
                 print("Cache is empty or refresh requested, fetching new data.")
-                
+        
+        # Fetch schema attestations
         raw_attestations = fetch_schema_attestations(schema_id)
+        if not raw_attestations:
+            # Cache the schema ID as having no data and return a 404 response
+            r.set(no_data_cache_key, 'true', ex=3600)
+            return jsonify({"message": "No data found for this schema ID."}), 404
+
         daoip7_schemas = populate_daoip7_compliant_schemas(context_schema_id)
         unique_attesters = {attestation['attester'] for attestation in raw_attestations}
 
         results = []
 
         for attester_address in unique_attesters:
-            attester_response, status_code = get_attestations(attester_address)  # Call and unpack the tuple
+            attester_response = get_attestations_new(attester_address)
+            status_code = attester_response[1]  # Extract status code from the response
 
             if status_code == 200:
-                attester_data = attester_response
+                attester_data = attester_response[0].get_json()  # Get JSON data from the response object
                 print("success")
 
                 # Filter schemas to include only those where the creator matches the attester address
@@ -523,6 +445,7 @@ def get_schema_attestations_new(schema_id, refresh=None):
                     "statusCode": status_code,
                     "attesterAddress": attester_address
                 })
+        
         # Cache the results for 1 hour
         r.set(cache_key, json.dumps(results), ex=3600)
         print("Data cached for 1 hour.")
@@ -530,6 +453,7 @@ def get_schema_attestations_new(schema_id, refresh=None):
         return jsonify(results), 200
     except Exception as e:
         return jsonify({"error in get_schema_attestations": str(e)}), 500
+
     
     
 
